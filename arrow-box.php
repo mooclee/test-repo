a0ac5cb421b99d2afc68891a875a4867df4e0988
alantypoon <alantypoon@gmail.com>
2017-02-10 06:09:59 +0800

    Add files via upload

---
+++ arrow-box.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <title>cssarrowplease</title>
+    <meta name="description" content="CSSArrowPlease allows you to create and export CSS code for a custom box with an arrow extending out from the side.">
+    <link href='//fonts.googleapis.com/css?family=Terminal+Dosis:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="arrow-box.css" />
+  </head>
+  <body>
+		<input type="radio" name="group1" id="r1" value="1" style="
+				width: 20px;
+				margin-right: 8px;
+		">
+		<label for="r1"></label>
+		
+		
+		<table>
+			<tr>
+				<td>
+					<input type="radio" name="group1" id="r2" value="1" style="
+							width: 20px;
+							margin-right: 8px;
+					"/ checked>
+			</td>
+			<td>
+				<label for="r2"><span class='arrow_box'><span>Correct</span></span></label>
+			</td>
+			</tr>
+		</table>
+		
+  </body>
+</html>
